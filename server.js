fc2a0c341e723ca8b5d69eb97db73b9936b2a704
Luka Mirosevic <luka@goonbee.com>
2014-04-13 18:27:47 +0200

    Added health check

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-var net = require('net');
+var net = require('net'),
+	http = require('http');
 
-var server = net.createServer(function (socket) {
-  socket.write('Echo server\r\n');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('777');
+}).listen(80);
+
+net.createServer(function (socket) {
+  socket.write('Welcome to echo server\r\n');
   socket.pipe(socket);
-});
-
-server.listen(1337, '127.0.0.1');
+}).listen(1337);
