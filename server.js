5ed757b8190552d091c0997cb517133fffdb4206
Luka Mirosevic <luka@goonbee.com>
2014-04-14 11:22:58 +0200

    additional server

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ http.createServer(function (req, res) {
 }).listen(80);
 
 net.createServer(function (socket) {
-  socket.write('Welcome to echo server\r\n');
+  socket.write('Welcome to echo server 443\r\n');
   socket.pipe(socket);
 }).listen(443);
+
+net.createServer(function (socket) {
+  socket.write('Welcome to echo server 56475\r\n');
+  socket.pipe(socket);
+}).listen(56475);
