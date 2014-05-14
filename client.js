ce68a6ddbc0a33ed86563e59dfcb15215d6e0539
Luka Mirosevic <luka@goonbee.com>
2014-05-14 08:01:27 +0200

    small updates

--- client.js
+++ client.js
@@ -13,13 +13,15 @@ connection.on('error', function(err) {
 var resultLogger = function(err, result) {
   console.log('----------');
   console.log(clc.blue(result));
-  console.log(clc.red(err));
+  console.log(clc.red(err ? err.status.toString() + ': ' + err : ''));
   console.log('----------');
 };
 
 // client.setChatOptions(userId, chatId, chatOptions, function(err, result) {
 //   resultLogger.apply(this, arguments);
 // });
+
+
 
 // // client.alive(resultLogger);
 client.isUsernameAvailable('luka', function(err, result) {
@@ -37,20 +39,17 @@ client.isUsernameAvailable('luka', function(err, result) {
     });
   }
   else {
-    whatToDo('537095499b54daf88a971199');
+    whatToDo('5371eca27ba5e6ce1bf04077');
   }
 });
 
 //what to do with our logged in user
 var whatToDo = function(userId) {
-  client.setChatOptions(userId, 'dogchat', new ttypes.ChatOptions({name: "luka's chat", topic: "all about dogs"}), resultLogger);
+  client.messages(userId, 'dogchat', new ttypes.Range({direction: ttypes.RangeDirection.FORWARDS, index: 0, length: 10}));
+
+  //foo try that one
+  // client.setChatOptions(userId, 'dogchat', new ttypes.ChatOptions({name: "luka's chat", topic: "all about dogs"}), resultLogger);
 
   // client.chats(ttypes.ChatSorting.PARTICIPANT_COUNT, new ttypes.Range({direction: ttypes.RangeDirection.FORWARDS, index: 0, length: 10}), resultLogger);
 };
-
-
-
-
-
-
 
